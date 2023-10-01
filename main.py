f0843ec83ab2551cbf31e29af8d3a21e536849fa
jackar4 <146545563+jackar4@users.noreply.github.com>
2023-10-01 18:01:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import yt_dlp
 from yt_dlp import YoutubeDL
 import os
 
-# Get the current directory of the script
+#getting the files directory
 current_directory = os.path.dirname(os.path.realpath(__file__))
 
-# Define the download options with the updated 'outtmpl' path
+# download options
 dl_ops = {
     'outtmpl': os.path.join(current_directory, 'downloads', '%(uploader)s', '%(title)s.%(ext)s')
 }
 
+#downloading the video
 videolink = input("Paste the video link: ")
 with YoutubeDL(dl_ops) as ydl:
     ydl.download([videolink])
